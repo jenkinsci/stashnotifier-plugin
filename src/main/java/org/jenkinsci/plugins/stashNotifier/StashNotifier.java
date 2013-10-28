b65b0d99111bbd14b5a84ffcf260526b19724533
Georg Gruetter <gruetter@gmail.com>
2013-10-28 21:22:35 +0100

    Trailing slashes on Stash root URL's are now removed. Fixes issue #18

--- src/main/java/org/jenkinsci/plugins/stashNotifier/StashNotifier.java
+++ src/main/java/org/jenkinsci/plugins/stashNotifier/StashNotifier.java
@@ -117,7 +117,9 @@ public class StashNotifier extends Notifier {
 			boolean ignoreUnverifiedSSLPeer,
 			String commitSha1,
 			boolean includeBuildNumberInKey) {
-		this.stashServerBaseUrl = stashServerBaseUrl;
+		this.stashServerBaseUrl = stashServerBaseUrl.endsWith("/")
+                ? stashServerBaseUrl.substring(0, stashServerBaseUrl.length()-2)
+                : stashServerBaseUrl;
 		this.stashUserName = stashUserName;
 		this.stashUserPassword = Secret.fromString(stashUserPassword);
 		this.ignoreUnverifiedSSLPeer
