4984f183a038d1c058333c32c25e503ce1f99465
René Scheibe <rene.scheibe@gmail.com>
2019-04-02 00:14:57 +0200

    Use @NonNull as in implemented interface

--- src/main/java/org/jenkinsci/plugins/stashNotifier/StashCredentialMatcher.java
+++ src/main/java/org/jenkinsci/plugins/stashNotifier/StashCredentialMatcher.java
@@ -4,12 +4,13 @@ import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsMatcher;
 import com.cloudbees.plugins.credentials.common.CertificateCredentials;
 import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
+import edu.umd.cs.findbugs.annotations.NonNull;
 
 /**
  * A very simple matcher to ensure we only show username/password or certificate credentials
  */
 public class StashCredentialMatcher implements CredentialsMatcher {
-    public boolean matches(Credentials credentials) {
+    public boolean matches(@NonNull Credentials credentials) {
         return (credentials instanceof CertificateCredentials) || (credentials instanceof UsernamePasswordCredentials);
     }
 }
