d22851c85373b1aabe123b8774256911c9fdbad8
Steve Hill <sghill.dev@gmail.com>
2021-10-24 12:46:12 -0700

    Allow specifying a custom HttpNotifierSelector
    Users can specify a system property to have a custom
    HttpNotifierSelector injected instead of the default.

--- src/main/java/org/jenkinsci/plugins/stashNotifier/StashNotifierModule.java
+++ src/main/java/org/jenkinsci/plugins/stashNotifier/StashNotifierModule.java
@@ -3,25 +3,57 @@ package org.jenkinsci.plugins.stashNotifier;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import hudson.Extension;
+import hudson.ExtensionList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Extension
 public class StashNotifierModule extends AbstractModule {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StashNotifierModule.class);
+
     @Override
     protected void configure() {
     }
 
     @Provides
     @Singleton
-    @Named("defaultApache")
-    HttpNotifier providesDefaultApacheHttpNotifier() {
-        return new DefaultApacheHttpNotifier();
+    @Named("fallback")
+    HttpNotifierSelector providesDefaultApacheHttpNotifierSelector() {
+        return new DefaultHttpNotifierSelector(new DefaultApacheHttpNotifier());
     }
 
     @Provides
-    HttpNotifierSelector providesHttpNotifierSelector(@Named("defaultApache") HttpNotifier httpNotifier) {
-        return new DefaultHttpNotifierSelector(httpNotifier);
+    List<HttpNotifierSelector> providesHttpNotifierSelectors() {
+        return ExtensionList.lookup(HttpNotifierSelector.class);
+    }
+
+    @Provides
+    @Singleton
+    HttpNotifierSelector providesHttpNotifierSelector(@Named("fallback") HttpNotifierSelector fallback,
+                                                      @Named("preferredHttpNotifierSelector") String preferredHttpNotifierSelector,
+                                                      List<HttpNotifierSelector> httpNotifierSelectors) {
+        HttpNotifierSelector selector = httpNotifierSelectors.stream()
+                .filter(s -> s.getClass().getName().equals(preferredHttpNotifierSelector))
+                .findFirst()
+                .orElse(fallback);
+        Class<? extends HttpNotifierSelector> selectedClass = selector.getClass();
+        if (selectedClass.getName().equals(preferredHttpNotifierSelector)) {
+            LOGGER.info("Using {}", selectedClass.getName());
+        } else {
+            LOGGER.warn("{} not found - using {}", preferredHttpNotifierSelector, selectedClass.getName());
+        }
+        return selector;
+    }
+
+    @Provides
+    @Singleton
+    @Named("preferredHttpNotifierSelector")
+    String providesPreferredHttpNotifierSelector() {
+        String override = HttpNotifierSelector.class.getName() + ".className";
+        return System.getProperty(override, DefaultApacheHttpNotifier.class.getName());
     }
 }
