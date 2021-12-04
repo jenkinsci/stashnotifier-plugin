18ae1a7aa2899dd7c9061ec6cb3c925890c2820e
Erkki Nokso-Koivisto <enoksoko@yahoo.com>
2021-12-04 16:21:29 +0200

    Personal Access Token can be provide using secret text Credential - small cleanup (instanceof should be using the interface instead of impl)

--- src/main/java/org/jenkinsci/plugins/stashNotifier/StashCredentialMatcher.java
+++ src/main/java/org/jenkinsci/plugins/stashNotifier/StashCredentialMatcher.java
@@ -5,7 +5,7 @@ import com.cloudbees.plugins.credentials.CredentialsMatcher;
 import com.cloudbees.plugins.credentials.common.CertificateCredentials;
 import com.cloudbees.plugins.credentials.common.UsernamePasswordCredentials;
 
-import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 
@@ -14,6 +14,6 @@ import edu.umd.cs.findbugs.annotations.NonNull;
  */
 public class StashCredentialMatcher implements CredentialsMatcher {
     public boolean matches(@NonNull Credentials credentials) {
-        return (credentials instanceof CertificateCredentials) || (credentials instanceof UsernamePasswordCredentials || (credentials instanceof StringCredentialsImpl));
+        return (credentials instanceof CertificateCredentials) || (credentials instanceof UsernamePasswordCredentials || (credentials instanceof StringCredentials));
     }
 }
